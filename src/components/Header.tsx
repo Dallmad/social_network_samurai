ff39e0d36c2e8d358ec2550bfa6cfd9f08d6aada
Dzmitry <maddal.privod@gmail.com>
2022-01-06 07:37:50 +0300

    create folder 'components' and file 'profile navbar header'

---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+export const Header = () => {
+    return <header className='header'>
+        <img src='https://game-tournaments.com/media/b/csbet_right_xray_2.png'/>
+    </header>
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+export const Profile =()=>{
+    return <div className='content'>
+        <div>
+            <img src='https://www.nibib.nih.gov/sites/default/files/2020-12/Abstract-Banner-730x250.jpg'/>
+        </div>
+        <div>
+            ava+descrition
+        </div>
+        <div>
+            My post
+            <div>
+                New post
+            </div>
+        </div>
+        <div>
+            Post 1
+        </div>
+        <div>
+            Post 2
+        </div>
+    </div>
+}
