28de9e21a3f156c15d91a26d15fe3fb8fb395d92
Dzmitry <maddal.privod@gmail.com>
2022-01-06 23:58:06 +0300

    create css modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
+import s from './Header.module.css'
+
 export const Header = () => {
-    return <header className='header'>
+    return <header className={s.header}>
         <img src='https://game-tournaments.com/media/b/csbet_right_xray_2.png'/>
     </header>
 }
