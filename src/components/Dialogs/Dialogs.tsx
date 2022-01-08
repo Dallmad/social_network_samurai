0a73411c41129d220f4f5448a89f0809dab19cb9
Dzmitry <maddal.privod@gmail.com>
2022-01-08 06:33:05 +0300

    added routing for Navbar.tsx

---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,11 @@
+import {DialogsPropsType} from "./DialogsPropsType";
+import s from './Dialogs.module.css';
+
+
+export const Dialogs = (props:DialogsPropsType) => {
+    return (
+        <div>
+            Dialogs
+        </div>
+    )
+}
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,7 +2,7 @@ import s from './Profile.module.css'
 import {MyPosts} from "./MyPosts/MyPosts";
 
 export const Profile = () => {
-    return <div className={s.content}>
+    return <div>
         <div>
             <img src='https://www.nibib.nih.gov/sites/default/files/2020-12/Abstract-Banner-730x250.jpg'/>
         </div>
