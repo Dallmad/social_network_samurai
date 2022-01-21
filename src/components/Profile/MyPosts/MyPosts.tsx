b0bd9b6c874a0ce61c5c5fcebec2db1be2b856ee
Dzmitry <maddal.privod@gmail.com>
2022-01-21 05:47:51 +0300

    used ref

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,6 +1,6 @@
 import s from './MyPosts.module.css'
 import {Post} from "./Post/Post";
-import React from "react";
+import React, {createRef, useRef} from "react";
 import {PostPropsType} from "../../../redux/state";
 
 type MyPostType={
@@ -8,16 +8,20 @@ type MyPostType={
 }
 
 export const MyPosts:React.FC<MyPostType> = (props) => {
-
     let postElements = props.posts.map( p=>
         <Post id={p.id} message={p.message} likesCount={p.likesCount}/>)
+    const newPostElement = React.createRef<HTMLTextAreaElement>();
+    let addPost = () => {
+        let text = newPostElement.current?.value
+      alert(text)
+    }
     return <div className={s.postBlock}>
         <h3> My post </h3>
             <div>
-                <textarea></textarea>
+                <textarea ref={newPostElement}></textarea>
             </div>
             <div>
-                <button>Add post</button>
+                <button onClick={addPost}>Add post</button>
             </div>
         <div className={s.posts}>
             {postElements}
