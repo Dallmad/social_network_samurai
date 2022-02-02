b775662d05bb37960bf535c570590c336a237b35
Dzmitry <maddal.privod@gmail.com>
2022-02-02 17:13:05 +0300

    created store

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,4 +1,3 @@
-import {rerenderEntireTree} from "../render";
 
 export type DialogItemPropsType = {
     id: number
@@ -17,42 +16,59 @@ export type stateType = {
     profilePage: { posts: PostPropsType[], messageForNewPost: string }
     dialogsPage: { dialogs: DialogItemPropsType[], messages: MessagePropsType[] }
 }
+export type StoreType = {
+    _state: stateType
+    addPostCallback:(post: string)=>void
+    changeNewTextCallback:(newText: string)=>void
+    _onChange:()=>void
+    subscribe:(callback:() => void)=>void
+    getState:() => stateType
+}
 
-export let state: stateType = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi, how are you?', likesCount: 11},
-            {id: 2, message: 'It"s my first post.', likesCount: 23},
-        ],
-        messageForNewPost: ''
+export const store:StoreType = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi, how are you?', likesCount: 11},
+                {id: 2, message: 'It"s my first post.', likesCount: 23},
+            ],
+            messageForNewPost: ''
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Dima'},
+                {id: 2, name: 'Andrey'},
+                {id: 3, name: 'Sveta'},
+                {id: 4, name: 'Sasha'},
+                {id: 5, name: 'Viktor'},
+                {id: 6, name: 'Valera'}
+            ],
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How is your it-kamasutra?'},
+                {id: 3, message: 'Yo'}
+            ]
+        }
     },
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: 'Dima'},
-            {id: 2, name: 'Andrey'},
-            {id: 3, name: 'Sveta'},
-            {id: 4, name: 'Sasha'},
-            {id: 5, name: 'Viktor'},
-            {id: 6, name: 'Valera'}
-        ],
-        messages: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'How is your it-kamasutra?'},
-            {id: 3, message: 'Yo'}
-        ]
+    addPostCallback (post: string) {
+        const newPost: PostPropsType = {
+            id: 5,
+            message: this._state.profilePage.messageForNewPost,
+            likesCount: 0
+        }
+        this._state.profilePage.posts.push(newPost)
+        this._state.profilePage.messageForNewPost=''
+        this._onChange()
+    },
+    changeNewTextCallback (newText: string) {   //name function on js samurai -
+        this._state.profilePage.messageForNewPost = newText   //updateNewPostText
+        this._onChange()
+    },
+    _onChange () {console.log('State changed')}, //_callSubscribers on js samurai
+    subscribe (callback:()=>void) {
+        this._onChange=callback
+    },
+    getState () {
+        return this._state
     }
-}
-export let addPost = (post: string) => {
-    const newPost: PostPropsType = {
-        id: 5,
-        message: state.profilePage.messageForNewPost,
-        likesCount: 0
-    }
-    state.profilePage.posts.push(newPost)
-    state.profilePage.messageForNewPost=''
-    rerenderEntireTree(state)
-}
-export let changeNewText = (newText: string) => {
-    state.profilePage.messageForNewPost = newText
-    rerenderEntireTree(state)
 }
