c7694e2ad475bf22272730c54f47dcd333976f09
Dzmitry <maddal.privod@gmail.com>
2022-02-03 18:38:02 +0300

    refactored styles

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -3,7 +3,7 @@ import s from './ProfileInfo.module.css'
 export const ProfileInfo = () => {
     return <div>
         <div>
-            <img className={s.img} src='https://www.nibib.nih.gov/sites/default/files/2020-12/Abstract-Banner-730x250.jpg'/>
+            <img className={s.img} src='https://www.meme-arsenal.com/memes/0a5bb7478b7e122896daece13d9c36e7.jpg'/>
         </div>
         <div className={s.descriptionBlock}>
             ava+descrition
