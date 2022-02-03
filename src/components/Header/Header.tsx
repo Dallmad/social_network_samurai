b9f8e217e91297440b03f49c3d79950c1e954662
Dzmitry <maddal.privod@gmail.com>
2022-02-03 18:57:03 +0300

    refactored styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,6 @@ import s from './Header.module.css'
 
 export const Header = () => {
     return <header className={s.header}>
-        <img src='https://game-tournaments.com/media/b/csbet_right_xray_2.png'/>
+        <img src='https://gazacademy.ru/wp-content/uploads/2019/04/loading-gears-animation-3.gif'/>
     </header>
 }
