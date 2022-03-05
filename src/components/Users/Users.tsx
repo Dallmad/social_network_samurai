8f7e7ee6769e30c2f2b184f483d9653063f027d1
Dzmitry <maddal.privod@gmail.com>
2022-03-05 23:40:40 +0300

    connecting axios

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,44 +1,24 @@
 import React from 'react';
 import {UsersPropsType} from './UsersContainer';
 import s from './users.module.css'
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/avatar.png'
 
 export const Users: React.FC<UsersPropsType> = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKyyVmxudAz9oz04mTAe4jJ-BJZtByh3oC2Q&usqp=CAU',
-                    followed: false,
-                    fullName: 'Dzmitry',
-                    status: 'I am a shark',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKyyVmxudAz9oz04mTAe4jJ-BJZtByh3oC2Q&usqp=CAU',
-                    followed: true,
-                    fullName: 'Sasha',
-                    status: 'I am a big shark',
-                    location: {city: 'Moscow', country: 'Russia'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKyyVmxudAz9oz04mTAe4jJ-BJZtByh3oC2Q&usqp=CAU',
-                    followed: false,
-                    fullName: 'Andrew',
-                    status: 'I am a small shark',
-                    location: {city: 'Kiev', country: 'Ukraine'}
-                }
-            ]
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items)
+        }
         )
     }
+
     return (
         <div>
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} className={s.userPhoto}/>
+                            <img src={u.photos.small ? u.photos.small : userPhoto} className={s.userPhoto}/>
                         </div>
                         <div>
                             {u.followed ?
@@ -58,7 +38,7 @@ export const Users: React.FC<UsersPropsType> = (props) => {
                     <span>
                         <span>
                             <div>
-                                {u.fullName}
+                                {u.name}
                             </div>
                             <div>
                                 {u.status}
@@ -66,10 +46,10 @@ export const Users: React.FC<UsersPropsType> = (props) => {
                         </span>
                         <span>
                             <div>
-                                {u.location.country}
+                                {'u.location.country'}
                             </div>
                             <div>
-                                {u.location.city}
+                                {'u.location.city'}
                             </div>
                          </span>
                     </span>
