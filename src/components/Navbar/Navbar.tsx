d9b71f86574750421bcb81963e21eb65ee3c03bc
Dzmitry <maddal.privod@gmail.com>
2022-03-14 00:25:47 +0300

    Use axios request 'get'

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,6 +11,9 @@ export const Navbar = () => {
             <NavLink to='/messages' className={ navData => navData.isActive ? s.active : s.item }>Messages</NavLink>
         </div>
         <div className={s.item}>
+            <NavLink to='/users' className={ navData => navData.isActive ? s.active : s.item }>Users</NavLink>
+        </div>
+        <div className={s.item}>
             <NavLink to='/news' className={ navData => navData.isActive ? s.active : s.item }>News</NavLink>
         </div>
         <div className={s.item}>
