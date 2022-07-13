a1cb6b4a729ef886b1de1f2b99a1f4663a505888
Dzmitry <maddal.privod@gmail.com>
2022-07-14 00:20:23 +0300

    Bug with no rerender users page when click follow/unfollow

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,6 +1,6 @@
 import s from './users.module.css';
 import userPhoto from '../../assets/images/img.png';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {UserType} from '../../redux/users-reducer';
 import {NavLink} from 'react-router-dom';
 import {PaginationFC} from '../common/pagination/Pagination';
@@ -19,9 +19,19 @@ type UsersFCPropsType = {
 
 export const Users: React.FC<UsersFCPropsType> = (props) => {
 
+/*    const [follow, setFollow] = useState(false)
+
+    const onClickUnfollow = (uId: number) => {
+        props.unfollow(uId)
+        setFollow(false)
+    }
+    const onClickFollow = (uId: number) => {
+        props.follow(uId)
+        setFollow(false)
+    }*/
     return (
         <div>
-            <PaginationFC onPagesChanged={props.onPagesChanged} />
+            <PaginationFC onPagesChanged={props.onPagesChanged}/>
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
@@ -32,13 +42,11 @@ export const Users: React.FC<UsersFCPropsType> = (props) => {
                         </div>
                         <div>
                             {u.followed ?
-                                <button disabled={props.followingInProgress.some(id => id===u.id)}
-                                    onClick={() => {props.unfollow(u.id)}
-                                    }
+                                <button disabled={props.followingInProgress.some(id => id === u.id)}
+                                        onClick={() => props.unfollow(u.id)}
                                 >Unfollow</button>
-                                :<button disabled={props.followingInProgress.some(id => id===u.id)}
-                                    onClick={() => {props.follow(u.id)}
-                                    }
+                                : <button disabled={props.followingInProgress.some(id => id === u.id)}
+                                          onClick={() => props.follow(u.id)}
                                 >Follow</button>}
                         </div>
                     </span>
